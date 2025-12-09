0ac6216e644a03cb3cf62422863579e3c9a8720a
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-09 15:08:44 -0300

    Bug Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Home from "./pages/home.tsx";
 
-function App() {
-    return (
-        <>
-            <h1>Hi</h1>
-        </>
-    )
+export default function App() {
+	return (
+		<Routes>
+			<Route path="/" element={<AppLayout />}>
+				<Route index element={<Home />} />
+			</Route>
+
+			<Route path="*" element={<h1>404</h1>} />
+		</Routes>
+	);
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import AppLayout from "./layouts/AppLayout";
-import Home from "./pages/home.tsx";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<AppLayout />}>
-					<Route index element={<Home />} />
-				</Route>
-
-				{/* 404 */}
-				<Route path="*" element={<h1>404</h1>} />
-
-			</Routes>
+			<App />
 		</BrowserRouter>
 	</React.StrictMode>
 );
