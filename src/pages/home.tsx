522048429cbd5eed0b2e439ab114d1739deef3a6
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-10 13:51:39 -0300

    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,11 @@ export default function Home() {
     return (
         <>
             <h1>Home</h1>
+            <div className="card highlight">
+                <h4>Lorem</h4>
+                <p>Lorem ipsum</p>
+            </div>
+
             <div className="card">
                 <h4>Lorem</h4>
                 <p>Lorem ipsum</p>
