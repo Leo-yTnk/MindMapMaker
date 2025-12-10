48c8743333cb63f7ae538ada7d54c5b468bd3053
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-10 13:35:52 -0300

    CSS Changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,12 @@
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            <div className="card">
+                <h4>Lorem</h4>
+                <p>Lorem ipsum</p>
+            </div>
+        </>
     );
 }
   
