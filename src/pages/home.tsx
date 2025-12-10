64e22ebae1da863b56e5be0ee2cdc7efe7be9667
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-10 16:23:20 -0300

    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,44 @@
 export default function Home() {
+    function getGreeting() {
+        const now = new Date();
+        const hour = now.getHours();
+    
+        if (hour < 12) {
+            return "Bom dia";
+        } else if (hour < 18) {
+            return "Boa tarde";
+        } else {
+            return "Boa noite";
+        }
+    }
+    
+    let greeting = getGreeting()
+    let username = "TestUser"
+    let usernameMsg = "!"
+
+    if (username) {
+        usernameMsg = `, ${username}!`
+    }
+    
+
     return (
         <>
             <h1>Home</h1>
-            <div className="card highlight">
-                <h3>Olá! Pronto para criar algo novo?</h3>
-                <button>Começar a Criar</button>
-            </div>
+            <div className="content">
+                <div className="card highlight">
+                    <h3><span className="greeting">{greeting}</span><span className="user-name">{usernameMsg}</span></h3>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/>
+                        </svg>
+                        <span>Começar a Criar</span>
+                    </button>
+                </div>
 
-            <div className="card">
-                <h3>Lorem</h3>
-                <p>Lorem ipsum</p>
+                <div className="card">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum</p>
+                </div>
             </div>
         </>
     );
