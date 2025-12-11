fed530d095c0ec8df3c100d248843d86c07e9e94
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-11 15:18:59 -0300

    .

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,6 +4,15 @@ export default function AppLayout() {
     return (
         <>
             <nav>
+                <button className="active">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                    </svg>
+                    <span>Novo</span>
+                </button>
+
+                <span className="separator"></span>
+
                 <div>
                     <Link to="/" className="btn">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
@@ -20,13 +29,6 @@ export default function AppLayout() {
                         <span>Biblioteca</span>
                     </Link>
                 </div>
-                
-                <button className="active">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-                    </svg>
-                    <span>Novo</span>
-                </button>
             </nav>
 
             <main>
