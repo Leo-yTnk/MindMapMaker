cbf1a904429dd7ffe377e73bba352576e7dbec53
Leo-yTnk <leonardo.yukio.tanaka@gmail.com>
2025-12-14 22:41:51 -0300

    .

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,9 +21,9 @@ const defaultUsername = "Léo";
 const heroActions: HeroAction[] = [
     /* Task */ { 
         iconPath: "M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z", 
-        secondaryiconPath: "", 
+        secondaryiconPath: "M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z", 
         title: "Tarefa", 
-        description: "Gerencie seus compromissos, provas e trabalhos", 
+        description: "Gerencie suas provas e seus trabalhos", 
         type: "task" 
     },
     
@@ -76,7 +76,7 @@ export default function Home() {
                         <p>O que você vai fazer hoje?</p>
                     </article>
 
-                    <article className="hero-data">
+                    <article className="hero-list">
                         <ul>
                             {heroActions.map((action) => (
                                 <li key={action.title}>
